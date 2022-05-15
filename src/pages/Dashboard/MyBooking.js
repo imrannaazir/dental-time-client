89de7ff232fcf098cc4c26622bc95d3a3e3dee15
imrannaazir <imrannaaziremon@gmail.com>
2022-05-15 12:36:14 +0600

    all booking data for a user successfully loaded

--- src/pages/Dashboard/MyBooking.js
+++ src/pages/Dashboard/MyBooking.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import Loading from '../Shared/Loading';
 
 const MyBooking = () => {
+    const [user, loading] = useAuthState(auth);
+    const [MyBooking, setMyBooking] = useState([]);
+    useEffect(() => {
+        (async function () {
+            const { data } = await axios.get(`http://localhost:5000/booking?email=${user?.email}`)
+            setMyBooking(data)
+        })()
+    }, [user?.email])
+    if (loading) return <Loading />
+
     return (
         <div className='mt-12  w-[90%] mx-auto'>
             <p className='text-2xl mb-8'>My Appointments</p>
@@ -12,6 +26,7 @@ const MyBooking = () => {
                     <thead className=''>
                         <tr>
                             <th>SL</th>
+                            <th>Email</th>
                             <th>SERVICE</th>
                             <th>DATE</th>
                             <th>TIME</th>
@@ -19,12 +34,17 @@ const MyBooking = () => {
                     </thead>
                     <tbody>
                         {/* <!-- row 1 --> */}
-                        <tr>
-                            <th>1</th>
-                            <td>Cy Ganderton</td>
-                            <td>Quality Control Specialist</td>
-                            <td>Blue</td>
-                        </tr>
+                        {
+                            MyBooking.map((booking, index) =>
+                                <tr>
+                                    <th>{index + 1}</th>
+                                    <td>{booking.patientEmail}</td>
+                                    <td>{booking.treatmentName}</td>
+                                    <td>{booking.date}</td>
+                                    <td>{booking.slot}</td>
+                                </tr>
+                            )
+                        }
 
                     </tbody>
                 </table>
